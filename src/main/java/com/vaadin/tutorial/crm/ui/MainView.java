450e568670f588bbf08cd0596d677fef4ec44e3f
nzivo <johnnnzivo@gmail.com>
2021-02-13 19:13:40 +0300

    Grid List View Items

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,18 +1,39 @@
 package com.vaadin.tutorial.crm.ui;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.ContactService;
 
 @Route("")
-@PWA(name = "Vaadin Application",
-        shortName = "Vaadin App",
-        description = "This is an example Vaadin application.",
+@PWA(name = "ETunes",
+        shortName = "E-Tunes App",
+        description = "This is Tunes Vaadin application.",
         enableInstallPrompt = true)
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    private ContactService contactService;
+    private Grid<Contact> grid = new Grid<>(Contact.class);
 
+    public MainView(ContactService contactService) {
+        this.contactService = contactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+        add(grid);
+        updateList();
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll());
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email", "status");
     }
 
 }
